7226968158a1312252c307515e2d993424c616ff
Your Name <you@example.com>
2024-06-29 16:08:02 +0500

    Added url updatet

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import SignUpCreateAPIView,  VerifyCodeAPIView, UpdateUserAPIView
+from users.views import SignUpCreateAPIView,  VerifyCodeAPIView, UpdateUserAPIView, UserAvatarUpdate, ResetPasswordView
 
 app_name = "users"
 
@@ -7,4 +7,6 @@ urlpatterns = [
   path('register/' , SignUpCreateAPIView.as_view(), name='users_reg'),
   path('verify/', VerifyCodeAPIView.as_view(), name='verify'),
   path('update/', UpdateUserAPIView.as_view(), name='update_user'),
+  path('update/avatar/', UserAvatarUpdate.as_view(), name='update_avatar'),
+  path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
 ]

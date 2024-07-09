51c0a1217f44258de7472822e6c0f0122d57fed5
Your Name <you@example.com>
2024-07-09 23:49:33 +0500

    as

--- conf/settings.py
+++ conf/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/Insta-Clon/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
